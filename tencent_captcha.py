753b4c4c7ae196961f9eae866945fef3dfa40c4a
Frederic <frederic.t.chan@gmail.com>
2019-07-23 22:03:08 +0800

    add tencent captcha verify fix
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- tencent_captcha.py
+++ tencent_captcha.py
@@ -27,3 +27,13 @@ class TencentCaptcha:
             return cls._verify(request.json["captcha_ticket"],
                                request.json["captcha_rand"],
                                request.json["remote_addr"])
+
+    #  identity 服务上线前这个方法要被 server 调，先留着
+    @classmethod
+    def verify_old(cls):
+        if not all(map(request.form.get, ["captcha-ticket", "captcha-rand"])):
+            return False
+        else:
+            return cls._verify(request.form["captcha-ticket"],
+                               request.form["captcha-rand"],
+                               request.remote_addr)
